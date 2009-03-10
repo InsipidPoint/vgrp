fafe467bc7d94db284b5019a04a0d139240188f6
Ankit Gupta <gankit@cs.stanford.edu>
2009-03-09 23:15:38 -0700

    changed

---
+++ Detector.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "cv.h"
+#include "cxcore.h"
+#include "highgui.h"
+
+typedef struct _features {
+  int face_size;
+  CvPoint face_position;
+  CvPoint nostril_positions[2];
+  CvPoint lip_positions[2];
+  CvPoint nose_bridge;
+  CvPoint pupils[2];
+  CvPoint eyebrow_ends[2];
+  double horiz_slope;
+  double vert_slope;
+} Features;
+
+class Detector {
+public:
+  Detector();
+  ~Detector();
+  
+  Features ColdStart(IplImage *img);
+  void TrackFeatures(IplImage *img, Features& features, double model[9][3], double theta[3]);
+	void SetupTracking(IplImage *img, Features& features);
+  void GetModel(Features& features, double model[9][3]);
+  // move to private later
+  void FitModel(Features& features, double model[9][3], double theta[3]);
+private:
+  void FindFace(IplImage *img, Features& features);
+  void FindLips(IplImage *face_img, Features& features);
+	void FindNostrils(IplImage *face_img, Features &features);
+	void FindNoseBridge(IplImage *face_img, Features& features);
+	void FindPupils(IplImage *face_img, Features& features);
+	void FindEyebrowEnds(IplImage *face_img, Features& features);
+	
+  void FindFaceCenter(Features& features);
+  
+  CvMemStorage* storage;
+  CvHaarClassifierCascade* cascade;
+	
+	IplImage *grey, *prev_grey, *pyramid, *prev_pyramid, *swap_temp;
+	
+	int win_size;
+	int MAX_COUNT;
+	CvPoint2D32f* points[2], *swap_points;
+	char* status;
+	int count;
+	int flags;
+};
---
+++ vgrp.cpp
@@ -0,0 +1,126 @@
+#include "Camera.h"
+#include "Detector.h"
+#include <cstdio>
+
+const char *WINDOW_NAME = "Display Window";
+
+void DrawFace(IplImage *img, Features &f, bool col = true) {
+  static CvScalar colors[] = 
+  {
+      {{0,0,255}},
+      {{0,128,255}},
+      {{0,255,255}},
+      {{0,255,0}},
+      {{255,128,0}},
+      {{255,255,0}},
+      {{255,0,0}},
+      {{255,0,255}}
+  };
+  
+  int radius;
+  radius = cvRound(f.face_size*0.5);
+  
+  if(col) {
+    cvCircle( img, f.face_position, radius, colors[0], 3, 8, 0 ); // draw face
+
+    // draw lips
+    cvCircle(img, cvPoint(f.lip_positions[0].x,f.lip_positions[0].y), 1, colors[1], 3, 8, 0);
+    cvCircle(img, cvPoint(f.lip_positions[1].x,f.lip_positions[1].y), 1, colors[1], 3, 8, 0);
+
+  	// draw nostrils
+  	cvCircle(img, cvPoint(f.nostril_positions[0].x,f.nostril_positions[0].y), 1, colors[0], 3, 8, 0);
+  	cvCircle(img, cvPoint(f.nostril_positions[1].x,f.nostril_positions[1].y), 1, colors[0], 3, 8, 0);
+
+  	// draw nose bridge
+  	cvCircle(img, cvPoint(f.nose_bridge.x,f.nose_bridge.y), 1, colors[2], 3, 8, 0);
+
+  	// draw pupils
+  	cvCircle(img, cvPoint(f.pupils[0].x,f.pupils[0].y), 1, colors[3], 3, 8, 0);
+  	cvCircle(img, cvPoint(f.pupils[1].x,f.pupils[1].y), 1, colors[3], 3, 8, 0);
+
+  	// draw eyebrow ends
+  	cvCircle(img, cvPoint(f.eyebrow_ends[0].x,f.eyebrow_ends[0].y), 1, colors[4], 3, 8, 0);
+  	cvCircle(img, cvPoint(f.eyebrow_ends[1].x,f.eyebrow_ends[1].y), 1, colors[4], 3, 8, 0);
+  } else {    
+//    cvCircle( img, f.face_position, radius, colors[0], 3, 8, 0 ); // draw face
+
+    // draw lips
+    cvCircle(img, cvPoint(f.lip_positions[0].x,f.lip_positions[0].y), 1, colors[5], 3, 8, 0);
+    cvCircle(img, cvPoint(f.lip_positions[1].x,f.lip_positions[1].y), 1, colors[5], 3, 8, 0);
+
+  	// draw nostrils
+  	cvCircle(img, cvPoint(f.nostril_positions[0].x,f.nostril_positions[0].y), 1, colors[5], 3, 8, 0);
+  	cvCircle(img, cvPoint(f.nostril_positions[1].x,f.nostril_positions[1].y), 1, colors[5], 3, 8, 0);
+
+  	// draw nose bridge
+  	cvCircle(img, cvPoint(f.nose_bridge.x,f.nose_bridge.y), 1, colors[5], 3, 8, 0);
+
+  	// draw pupils
+  	cvCircle(img, cvPoint(f.pupils[0].x,f.pupils[0].y), 1, colors[5], 3, 8, 0);
+  	cvCircle(img, cvPoint(f.pupils[1].x,f.pupils[1].y), 1, colors[5], 3, 8, 0);
+
+  	// draw eyebrow ends
+  	cvCircle(img, cvPoint(f.eyebrow_ends[0].x,f.eyebrow_ends[0].y), 1, colors[5], 3, 8, 0);
+  	cvCircle(img, cvPoint(f.eyebrow_ends[1].x,f.eyebrow_ends[1].y), 1, colors[5], 3, 8, 0);
+  }
+}
+
+int main(int argc, char **argv) {
+  const char *filename = NULL;
+  if(argc > 1)
+    filename = argv[1];
+  
+  cvNamedWindow (WINDOW_NAME, CV_WINDOW_AUTOSIZE);
+  
+  const IplImage *current_frame;
+  IplImage *gray = cvCreateImage(cvSize(640,480), 8, 1);
+  IplImage *small_img = cvCreateImage(cvSize(640,480), 8, 3);
+  Camera cam(filename);
+  Detector detector;
+  Features f;
+  bool track = false;
+  double model[9][3];
+  double theta[3] = {0,0,0};
+  
+  while((current_frame = cam.GetFrame())) {
+    cvResize(current_frame, small_img, CV_INTER_LINEAR);
+    cvFlip(small_img, small_img, 1);
+    cvCvtColor(small_img, gray, CV_BGR2GRAY);
+    cvEqualizeHist(gray,gray);
+    
+    if(track) {
+      // tmp
+//      f = detector.ColdStart(gray);
+      
+      detector.TrackFeatures(gray, f, model, theta);
+      DrawFace(small_img, f, false);
+      detector.FitModel(f, model, theta);
+      
+      CvFont font;
+      double hScale=0.5;
+      double vScale=0.5;
+      int    lineWidth=1;
+      cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX, hScale,vScale,0,lineWidth);
+      char buf[100];
+      sprintf(buf,"t1:%f t2:%f t3:%f",theta[0],theta[1],theta[2]);
+      cvPutText (small_img,buf,cvPoint(200,400), &font, cvScalar(255,255,0));
+    } else {
+      f = detector.ColdStart(gray);
+    }
+    
+    if(f.face_size) {
+      DrawFace(small_img, f);
+    }
+
+    cvShowImage(WINDOW_NAME, small_img);
+	  if(cvWaitKey(10) == 't') {
+		  track = true;
+		  detector.GetModel(f, model);
+		  detector.SetupTracking(gray,f);
+	  }
+  }
+  
+  cvReleaseImage( &gray );
+  cvReleaseImage( &small_img );
+  return 0;
+}
